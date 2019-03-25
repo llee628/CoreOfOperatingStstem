93fa13c51f6845026b9f5235139e768ff1db80ca
llee628 <gn01672397@gmail.com>
2019-03-25 02:11:11 -0500

    fix some magic number in tests.c

--- student-distrib/tests.c
+++ student-distrib/tests.c
@@ -457,9 +457,9 @@ void launch_tests(){
 	//TEST_OUTPUT("test_deref_above_kernel", test_deref_above_kernel());
 
 	// ------ Check point 2
-    TEST_OUTPUT("test_rtc_close", test_rtc_close());
-    TEST_OUTPUT("test_rtc_write_open", test_rtc_write_open());
-    TEST_OUTPUT("test_rtc_read", test_rtc_read());
+    //TEST_OUTPUT("test_rtc_close", test_rtc_close());
+    //TEST_OUTPUT("test_rtc_write_open", test_rtc_write_open());
+    //TEST_OUTPUT("test_rtc_read", test_rtc_read());
 	//TEST_OUTPUT("test_rtc_set_pi_freq", test_rtc_set_pi_freq());
 	//TEST_OUTPUT("test_dir_read", test_dir_read());
 	//TEST_OUTPUT("test_frame0_file", test_frame0_file());
